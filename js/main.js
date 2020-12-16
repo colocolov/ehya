d7015c4450d006e7d517be2c5c80e901e8df8baf
Colocolov Stas <colocolov.stas@gmail.com>
2020-12-16 15:51:47 +0200

    cr: фильтр для брендов на JS

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("menu-button_active");
 });
 //-----
+
+// фильтр трендов
+let articles = document.querySelectorAll(".trends__good");
+// console.log(articles);
+// let filter = document.querySelector(".trends__tabs");
+// console.log(filter);
+// console.log(type);
+
+function doAction(value) {
+  // let type = document.querySelector(".trends__type").classList.toggle("trends__type_active");
+  // console.log(value);
+  // console.log(this.value);
+  for (let article of articles) {
+    if (article.dataset.category !== value) {
+      article.classList.add("trends__hide");
+      // type.classList.add("trends__type_active");
+    } else {
+      article.classList.remove("trends__hide");
+    }
+  }
+}
+//-----
